b33c672db04634c12868eea9256004d6e9798088
Daniel Cardenas <edudan@yahoo.com>
2018-07-12 09:23:21 -0500

    generic dao

--- mydoctor/src/main/java/com/solutions/base/mvc/dao/GenericDao.java
+++ mydoctor/src/main/java/com/solutions/base/mvc/dao/GenericDao.java
@@ -1,4 +1,4 @@
-package com.citi.isawtools.mvc.dao;
+package com.solutions.base.mvc.dao;
 import java.util.List;
 
 
@@ -17,31 +17,17 @@ public interface GenericDao<ENTITY,ID> {
 	
 	
 	public boolean existById(ID id);
-	public boolean existByIdPromote(ID id);
 	public boolean existBySimpleNaturalId(Object naturalIdValue);
-	public boolean existBySimpleNaturalIdPromote(Object naturalIdValue);
 	public ENTITY findById(ID id);
-	public ENTITY findByIdPromote(ID id);
 	public ENTITY findBySimpleNaturalId(Object naturalIdValue);
-	public ENTITY findBySimpleNaturalIdPromote(Object naturalIdValue);
 	public ENTITY getEntityBySimpleNaturalId(Object naturalIdValue);
-	public ENTITY getEntityBySimpleNaturalIdPromote(Object naturalIdValue);
 	public ENTITY merge(ENTITY entity);
-	public ENTITY mergePromote(ENTITY entity);
 	public ENTITY save(ENTITY entity);
-	public ENTITY savePromote(ENTITY entity);
 	public List<ENTITY> findAll();
 	public List<ENTITY> findAllOrderByColumnName(String columnMetaName);
-	public List<ENTITY> findAllOrderByColumnNamePromote(String columnMetaName);
 	public List<ENTITY> findAllOrderByNaturalId();
-	public List<ENTITY> findAllOrderByNaturalIdPromote();
-	public List<ENTITY> findAllPromote();	
 	public long getStatisticsCache();
-	public long getStatisticsCachePromote();
-	public void clear();   
-	public void clearPromote();   
+	public void clear();      
 	public void delete(ENTITY entity);
-	public void deletePromote(ENTITY entity);
 	public void flush();
-	public void flushPromote();
 }
--- mydoctor/src/main/java/com/solutions/base/mvc/dao/GenericDaoImpl.java
+++ mydoctor/src/main/java/com/solutions/base/mvc/dao/GenericDaoImpl.java
@@ -1,4 +1,4 @@
-package com.citi.isawtools.mvc.dao;
+package com.solutions.base.mvc.dao;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
@@ -25,10 +25,6 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 	@Qualifier(value = "sessionFactory")
 	SessionFactory sessionFactory;
 
-	@Autowired
-	@Qualifier(value = "sessionFactoryPromote")
-	SessionFactory sessionFactoryPromote;
-
 	protected Class<? extends ENTITY> persistentClass;
 
 	@SuppressWarnings({ "unchecked" })
@@ -47,15 +43,6 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 	protected String getNaturalId() {
 
 		ClassMetadata classMetadata = sessionFactory.getClassMetadata(persistentClass);
-		final int naturalIdAttributePosition = classMetadata.getNaturalIdentifierProperties()[0];
-		String identifierPropertyName = classMetadata.getPropertyNames()[naturalIdAttributePosition];
-
-		return identifierPropertyName;
-	}
-
-	protected String getNaturalIdPromote() {
-
-		ClassMetadata classMetadata = sessionFactoryPromote.getClassMetadata(persistentClass);
 		final int naturalIdAttributePosition = classMetadata.getNaturalIdentifierProperties()[0];
 		String identifierPropertyName = classMetadata.getPropertyNames()[naturalIdAttributePosition];
 
@@ -85,22 +72,7 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 		}
 		
 		return entity;
-		/*
-
-		@SuppressWarnings("unchecked")
-		ENTITY entity = (ENTITY) currentSession().bySimpleNaturalId(persistentClass).load(naturalIdValue);
-		if (entity == null) {
-			try {
-				entity = persistentClass.newInstance();
-			} catch (InstantiationException | IllegalAccessException e) {
-				return entity;
-			}
-
-		}
-		return entity;
-		*/
-		
-		
+	
 
 	}
 
@@ -167,12 +139,10 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 	
 	
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public ENTITY findById(ID id) {
 
 		return (ENTITY) currentSession().get(persistentClass, id);
-	//	return (ENTITY) currentSession().createCriteria(persistentClass).setCacheable(true).add(Restrictions.idEq(id)).uniqueResult();
 
 		
 	}
@@ -206,12 +176,10 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 	@SuppressWarnings("unchecked")
 	@Override
 	public ENTITY findBySimpleNaturalId(Object naturalIdValue) {
-		//return null to return an empty entity use other method
 		
 		ENTITY entity = (ENTITY) currentSession().createCriteria(persistentClass).add( Restrictions.eq(this.getNaturalId(), naturalIdValue)).uniqueResult();
 
 		return entity;
-	//		return (ENTITY) currentSession().bySimpleNaturalId(persistentClass).load(naturalIdValue);
 
 	}
 
@@ -236,159 +204,6 @@ public abstract class GenericDaoImpl<ENTITY, ID extends Serializable> implements
 		return (List<ENTITY>) criteria.list();
 
 	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public ENTITY getEntityBySimpleNaturalIdPromote(Object naturalIdValue) {
-		
-	ENTITY entity = (ENTITY) currentSessionPromote().createCriteria(persistentClass).add( Restrictions.eq(this.getNaturalIdPromote(), naturalIdValue)).uniqueResult();
-    if (entity == null) {
-		try {
-			entity = persistentClass.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			return entity;
-		}
-
-	}
-		return entity;
-
-		
-		/*
-
-		ENTITY entity = (ENTITY) currentSessionPromote().bySimpleNaturalId(persistentClass).load(naturalIdValue);
-		if (entity == null) {
-			try {
-				entity = persistentClass.newInstance();
-			} catch (InstantiationException | IllegalAccessException e) {
-				return entity;
-			}
-
-		}
-		return entity;
-*/
-	}
-
-	protected Session currentSessionPromote() {
-
-		return sessionFactoryPromote.getCurrentSession();
-	}
-
-	@Override
-	public ENTITY mergePromote(ENTITY entity) {
-
-		currentSessionPromote().merge(entity);
-		return entity;
-	}
-
-	@Override
-	public ENTITY savePromote(ENTITY entity) {
-
-		currentSessionPromote().saveOrUpdate(entity);
-		return entity;
-	}
-
-	@Override
-	public void deletePromote(ENTITY entity) {
-
-		currentSessionPromote().delete(entity);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public ENTITY findByIdPromote(ID id) {
-
-		return (ENTITY) currentSessionPromote().get(persistentClass, id);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public ENTITY findBySimpleNaturalIdPromote(Object naturalIdValue) {
-		//return null to return an empty entity use other method
-		ENTITY entity = (ENTITY) currentSessionPromote().createCriteria(persistentClass).add( Restrictions.eq(this.getNaturalIdPromote(), naturalIdValue)).uniqueResult();
-		
-		return entity;
-
-
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<ENTITY> findAllOrderByNaturalIdPromote() {
-
-		Criteria criteria = currentSessionPromote().createCriteria(persistentClass);
-
-		Order order = Order.asc(this.getNaturalIdPromote()).ignoreCase();
-		criteria.addOrder(order);
-
-		return (List<ENTITY>) criteria.list();
-
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<ENTITY> findAllOrderByColumnNamePromote(String columnMetaName) {
-
-		Criteria criteria = currentSessionPromote().createCriteria(persistentClass);
-
-		Order order = Order.asc(columnMetaName).ignoreCase();
-		criteria.addOrder(order);
-
-		return (List<ENTITY>) criteria.list();
-
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<ENTITY> findAllPromote() {
-
-		return (List<ENTITY>) currentSessionPromote().createCriteria(persistentClass).list();
-
-	}
-
-	@Override
-	public void flushPromote() {
-
-		currentSessionPromote().flush();
-	}
-
-	@Override
-	public void clearPromote() {
-
-		currentSessionPromote().clear();
-	}
-
-	@Override
-	public boolean existByIdPromote(ID id) {
-
-		ENTITY entity = this.findByIdPromote(id);
-		if (entity == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	@Override
-	public boolean existBySimpleNaturalIdPromote(Object naturalIdValue) {
-
-		ENTITY entity = this.findBySimpleNaturalIdPromote(naturalIdValue);
-		if (entity == null) {
-			return false;
-		} else {
-			return true;
-		}
-
-	}
-
-	@Override
-	public long getStatisticsCachePromote() {
-
-		return sessionFactoryPromote.getStatistics().getSecondLevelCacheHitCount();
-	}
-
-	
-	
-	
 	
 	
 	@Override

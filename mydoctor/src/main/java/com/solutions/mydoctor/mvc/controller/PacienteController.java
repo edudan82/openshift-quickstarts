67253dba1b014bc6eaeeb739655582d9d39c93d6
edudan82 <edudan@yahoo.com>
2018-07-18 16:43:47 -0500

    Add files via upload

--- mydoctor/src/main/java/com/solutions/mydoctor/mvc/controller/PacienteController.java
+++ mydoctor/src/main/java/com/solutions/mydoctor/mvc/controller/PacienteController.java
@@ -1,8 +1,11 @@
 package com.solutions.mydoctor.mvc.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,6 +29,52 @@ public class PacienteController {
 		return "pacienteAgregar";
 
 	}
+	
+	
+	@RequestMapping(value="/pacienteEditar", method=RequestMethod.GET)
+	public String goPacienteEditar(Model model){
+	
+		model.addAttribute("paciente", dummyPaciente());
+		
+		return "pacienteAgregar";
+
+	}
+	
+	
+	@RequestMapping(value="/pacienteBuscar", method=RequestMethod.GET)
+	public String goPacienteBuscar(Model model){
+	
+		model.addAttribute("paciente", dummyPacienteList());
+		
+		return "pacienteBuscar";
+
+	}
+	
+	
+	public Paciente dummyPaciente(){
+		
+		Paciente paciente = new Paciente();
+		paciente.setApellidos("cardenas");
+		paciente.setNombre("Daniel");
+		paciente.setFechaNacimiento(LocalDate.now().minusYears(18));
+		paciente.setSexo("M");
+		return paciente;
+	}
+	
+	
+	
+	public List<Paciente> dummyPacienteList(){
+		List<Paciente> pacienteList = new ArrayList<Paciente>();
+		Paciente paciente = new Paciente();
+		paciente.setApellidos("cardenas");
+		paciente.setNombre("Daniel");
+		paciente.setFechaNacimiento(LocalDate.now().minusYears(18));
+		paciente.setSexo("M");
+		
+		pacienteList.add(paciente);
+		return pacienteList;
+	}
+	
 	
 
 
